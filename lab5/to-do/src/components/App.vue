71d9c2d91cdc976662038324809d5890e33bc7de
Abbazov Valerian <asr77734@gmail.com>
2021-03-11 15:54:53 +0500

    lab 5 working

---
+++ lab5/to-do/src/components/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <Page  actionBarHidden="true">
+        <ScrollView>
+          <StackLayout>
+            <TextField v-model="valueInput" @returnPress= "add()"/>
+            <ListView for="(task, index) in tasks" :key="index">
+              <v-template>
+                <Label :text="task.title" />
+              </v-template>
+            </ListView>
+          </StackLayout>
+        </ScrollView>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        valueInput: '',
+        tasks: [],
+        completeTasks: []
+      }
+    },
+    methods:{
+        add: function(){    
+            if(this.valueInput === '') { return };
+            this.tasks.push({
+                title: this.valueInput,
+                id: Math.random()
+            });
+            this.valueInput = '';
+        },
+        del: function(index, type){
+            if (type === 'work' ) this.tasks.splice(index,1);
+            else this.completeTasks.splice(index,1);
+        },
+        check: function(index){    
+            this.completeTasks.push(...this.tasks.splice(index, 1));
+        }
+    }
+  }
+</script>
+
+<style scoped>
+   
+</style>
